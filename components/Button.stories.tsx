110010771e30c6bdfdb95327b637ad1eca6871b8
AlexanderLapygin <AlexanderLapygin@gmail.com>
2019-11-11 21:16:38 +0300

    feat: initial committing

---
+++ components/Button.stories.tsx
@@ -0,0 +1,8 @@
+// components/Button.stories.tsx
+import * as React from 'react'
+import { storiesOf } from '@storybook/react'
+import Button from './Button'
+
+storiesOf('Button', module).add('with text', () => {
+    return <Button buttonText="Hello World" />
+})
---
+++ components/Button.tsx
@@ -0,0 +1,8 @@
+// components/Button.tsx
+import * as React from 'react'
+
+type Props = {
+    buttonText: string
+}
+
+export default (props: Props) => <button>{props.buttonText}</button>
